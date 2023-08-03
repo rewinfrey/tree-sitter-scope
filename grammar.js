e7a304a60b525ca9eeaf026ddc7f8b319434feca
Rick Winfrey <rewinfrey@github.com>
2023-08-02 21:16:05 -0700

    Rename to curly_scope

--- grammar.js
+++ grammar.js
@@ -10,14 +10,14 @@ module.exports = grammar({
     scope: $ => prec.left(
       repeat1(
         choice(
-          $._curl_scope,
+          $._curly_scope,
           $._paren_scope,
           $._bracket_scope,
         ),
       )
     ),
 
-    _curl_scope: $ => prec.left(1, seq('{', repeat($.scope), '}')),
+    _curly_scope: $ => prec.left(1, seq('{', repeat($.scope), '}')),
     _paren_scope: $ => prec.left(1, seq('(', repeat($.scope), ')')),
     _bracket_scope: $ => prec.left(1, seq('[', repeat($.scope), ']')),
   }
